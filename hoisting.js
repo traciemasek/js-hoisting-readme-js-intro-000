8ae527830560289caa0e72c7d694b3088e9882a9
Tracie Masek <traciemasek@gmail.com>
2018-11-13 19:51:47 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -9,9 +9,6 @@ function callMe() {
 
 
 function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
-
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
@@ -19,13 +16,11 @@ function crazy() {
 }
 
 function sayMyName() {
-  // fix the code in here:
   var name = "Cricky";
-
   sayMy();
 
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
   }
 }
